bb2b47ce77e203b8d7a7d396798767bf53606368
vphuc0601 <nguyenvanphuc0601@gmail.com>
2021-05-20 08:48:10 +0700

    lucky

---
+++ Modul4/14_Security/demo-security-master/src/main/java/com/trungdoan/demosecurity/service/RoleService.java
@@ -0,0 +1,11 @@
+package com.trungdoan.demosecurity.service;
+
+import com.trungdoan.demosecurity.model.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    Role findByName(String name);
+    List<Role> findAll();
+    Role findById(long id);
+}
